7bc07c35cbb4abd2b7ccc3a700799c8a3813cad1
SofeaNoh <sofeamnoh@gmail.com>
2025-01-10 09:34:05 +0800

    first commit

---
+++ ecommerce.py
@@ -0,0 +1,12 @@
+#%% 1. Import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+#%% 2. Defining Constant
+print(os.getcwd())
+# CSV_PATH = 
+
+
+#%% Data Loading
